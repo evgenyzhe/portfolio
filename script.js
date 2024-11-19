e2d21a1d6d351b5504dcbfb6c7b925bf1bc2e4a9
evgenyzhe <zhesides@gmail.com>
2024-11-19 17:41:21 +0300

    New fix

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Welcome to My Portfolio!");
+// Получаем текущий URL-адрес
+const currentUrl = window.location.pathname;
+
+// Получаем все ссылки в хэдере
+const navLinks = document.querySelectorAll('nav ul li a');
+
+// Добавляем класс 'active' той ссылке, которая соответствует текущему URL
+navLinks.forEach(link => {
+    if (link.getAttribute('href') === currentUrl.split('/').pop()) {
+        link.classList.add('active');
+    }
+});
